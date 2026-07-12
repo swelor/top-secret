2670f99d135cb6f44b790ff4a27c70edfb93c6a1
swelor <278988358+swelor@users.noreply.github.com>
2026-07-12 17:32:30 +0200

    added animations to navbar icons

---
+++ src/navbar/animations.js
@@ -0,0 +1,8 @@
+export const iconAnimation = {
+    translateX: ['0%', '10%'],
+    opacity: [1, 0.5],
+    easing: 'linear',
+    duration: 300,
+};
+
+
--- src/navbar/bar.jsx
+++ src/navbar/bar.jsx
@@ -1,6 +1,8 @@
 import './navbar.css'
 import { auth } from './firebase'
 import { signOut } from 'firebase/auth'
+import { Anime } from 'react-anime'
+import { iconAnimation } from './animations.js'
 
 export default function Navbar({ onLetterboxOpen, onProfileClick }) {
     const handleLogout = async () => {
@@ -13,20 +15,28 @@ export default function Navbar({ onLetterboxOpen, onProfileClick }) {
                 <span>💌</span>
             </div>
             <div className='navbar-buttons'>
-                {/*<button className='nav-btn' title='profile' onClick={onProfileClick}>
+                <Anime animation={iconAnimation}>
+                <button className='nav-btn' title='profile' onClick={onProfileClick}>
                     👤
                 </button>
+                </Anime>
         <button className='nav-btn' title='where is bub?'>
                     📍
             </button>
                 <button className='nav-btn' title='ping bub'>
+                <Anime animation={iconAnimation}>
                     🔔
-                </button>*/}
+                </Anime>
+                </button>
                 <button className='nav-btn' onClick={onLetterboxOpen} title='write a letter back'>
+                <Anime animation={iconAnimation}>
                     📬
+                </Anime>
                 </button>
                 <button className='nav-btn' title='logout' onClick={handleLogout}>
+                <Anime animation={iconAnimation}>
                     🚪
+                </Anime>
                 </button>
             </div>
         </nav>
